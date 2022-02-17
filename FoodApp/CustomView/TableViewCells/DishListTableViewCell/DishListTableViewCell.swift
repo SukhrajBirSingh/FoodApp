67b785be14005a4b4195fde442a9d32973abb059
Sukhraj Bir Singh <sgosal15@gmail.com>
2022-02-16 22:39:57 -0700

    place order

---
+++ FoodApp/CustomView/TableViewCells/DishListTableViewCell/DishListTableViewCell.swift
@@ -0,0 +1,33 @@
+//
+//  DishListTableViewCell.swift
+//  FoodApp
+//
+//  Created by Admin on 2022-02-15.
+//
+
+import UIKit
+
+class DishListTableViewCell: UITableViewCell {
+    
+    static let identifier = "DishListTableViewCell"
+
+    @IBOutlet weak var dishImageView: UIImageView!
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
+    
+    func setup(dish : Dish) {
+        dishImageView.kf.setImage(with: dish.image?.asUrl)
+        titleLabel.text = dish.name
+        descriptionLabel.text = dish.description
+    }
+    
+    func setup (order : Order){
+        dishImageView.kf.setImage(with: order.dish?.image?.asUrl)
+        titleLabel.text = order.dish?.name
+        descriptionLabel.text = order.name
+    }
+    
+}
